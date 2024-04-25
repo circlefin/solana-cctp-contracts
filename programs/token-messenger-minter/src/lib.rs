4e8f1d7bd4c95c3e1f4cb8ba2118897b7163c7c4
Chase McDermott <chase.mcdermott@circle.com>
2024-04-25 09:58:15 -0500

    Update security_txt (#15)
    Update security fields

--- programs/token-messenger-minter/src/lib.rs
+++ programs/token-messenger-minter/src/lib.rs
@@ -28,10 +28,8 @@ use {anchor_lang::prelude::*, token_messenger::*, token_minter::*};
 solana_security_txt::security_txt! {
     name: "Token Messenger and Minter for CCTP",
     project_url: "https://github.com/circlefin/solana-cctp-contracts",
-    contacts: "security@circle.com",
-    policy: "",
-    preferred_languages: "en",
-    auditors: "Halborn"
+    contacts: "link:https://hackerone.com/circle",
+    policy: "https://github.com/circlefin/solana-cctp-contracts/blob/master/SECURITY.md"
 }
 
 declare_id!("CCTPiPYPc6AsJuwueEnWgSgucamXDZwBd53dQ11YiKX3");
